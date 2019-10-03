1d4b5180554c2841bd46b852e940a75a773b0845
Krzysztof Gruca <chris.gruca@tlen.pl>
2019-10-03 21:32:02 +0200

    "Added exercise 1"
    On branch master

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program do zadania 1");
+            Console.Write("Podaj a: ");
+
+            string aIn = Console.ReadLine();
+
+            Console.Write("Podaj b: ");
+
+            string bIn = Console.ReadLine();
+
+            float a = float.Parse(aIn);
+            float b = float.Parse(bIn);
+
+            float m, n;
+
+            m = a*a;
+            m += b;
+            n = a + b;
+            n *= n;
+            if (n == 0) Console.WriteLine("Nie dziel przez 0!");
+            else 
+            {
+                m /= n;
+
+                Console.WriteLine("Wartość wyrażenia to:" + m);
+            }
+        }
+    }
+}
